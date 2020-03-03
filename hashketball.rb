597f87bbef3ed7b42d98618cf508fe422e23ed34
Josh Allen <joshmrallen@gmail.com>
2020-03-03 18:36:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -41,7 +41,7 @@ def game_hash
           :slam_dunks => 15
         }, 
         {
-          :player_name => "",
+          :player_name => "Mason Plumlee",
           :number => ,
           :shoe => ,
           :points => ,
