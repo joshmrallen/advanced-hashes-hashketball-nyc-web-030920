8877cd6cfc0cfbe8dbad3bec2d343a56ee7ef9ad
Josh Allen <joshmrallen@gmail.com>
2020-03-03 18:33:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,16 +6,124 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets", #the team's name as a string
       :colors => ["black", "white"],    #individual color names in an array
-      :players => [{}, {}, {}, {}, {}]    #an array of hashes - contains stats for each player (including name)
-    },
+      :players => [
+        {
+          :player_name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        }, 
+        {
+          :player_name => "Reggie Evans",
+          :number => 0,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        }, 
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        }, 
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        }, 
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        }]    #an array of hashes - contains stats for each player (including name)
+    }, #home team hash
     :away => {
       :team_name => "Charlotte Hornets", #the team's name as a string
       :colors => ["turquoise", "purple"],    #individual color names in an array
-      :players => []    #an array of hashes - contains stats for each player (including name)
-    }
+      :players => [
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        }]    #an array of hashes - contains stats for each player (including name)
+    } #away team hash
   }
-  
-  
   
   return hash
   
