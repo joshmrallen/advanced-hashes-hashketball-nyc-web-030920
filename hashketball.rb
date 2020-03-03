8d6e3eaab6000cb2de2b4ccdf603866385ebe943
Josh Allen <joshmrallen@gmail.com>
2020-03-03 19:23:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,9 +136,9 @@ end
 def num_points_scored(player_name)
   
   info = game_hash
-  binding.pry
+  
   #using the index of the players array of hashes: info[:home][:players][index][:points]
-  if info[:home][:players].index{|name| name == player_name}
+  if info[:home][:players].index{|player| player.any?{|key, value| value == player_name}}
     return info[:home][:players][
       info[:home][:players].index{|name| name == player_name}
       ][:points]
@@ -147,7 +147,6 @@ def num_points_scored(player_name)
         info[:away][:players].index{|name| name == player_name}
         ][:points]
   end
-  binding.pry
   
 end
 
