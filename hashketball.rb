e7cb28c3c677fea7b8e38f428788d20b40bf2f2a
Josh Allen <joshmrallen@gmail.com>
2020-03-03 18:25:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,8 @@ def game_hash
   
   hash = {
     :home => {
-      :team_name => "", #the team's name as a string
-      :colors => [],    #individual color names in an array
+      :team_name => "Brooklyn Nets", #the team's name as a string
+      :colors => ["black", "white"],    #individual color names in an array
       :players => []    #an array of hashes - contains stats for each player (including name)
     },
     :away => {
