854ddefb84dc5c79edbf8a8ccf7ce0e939c3865b
Josh Allen <joshmrallen@gmail.com>
2020-03-03 21:35:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -268,7 +268,7 @@ def max_stat(stat)
     
   }
   
-  info[:home][:players].each{|hash| hash.fetch(stat)}
+  info[:home][:players].map{|hash| hash.fetch(stat)}
   
   
   return stat_value #a 2-key hash with name and stat
