598928145a005841a9949d0b13ff8163ec8d6d36
Josh Allen <joshmrallen@gmail.com>
2020-03-03 21:29:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -268,7 +268,7 @@ def max_stat(stat)
     
   }
   
-  if info[:home][:players].index{|player| player.any?{|key, value| value == player_name}}
+  info[:home][:players].index{|player| player.any?{|key, value| value == player_name}}
   
   
   return stat_value #a 2-key hash with name and stat
